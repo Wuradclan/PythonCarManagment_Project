e390b70bb1178da01f788db446222a4b0c89a3ff
WuRAD <morad.aitabdellah@gmail.com>
2022-06-06 16:44:31 -0400

    search fucntion fixed and tested

--- CarManagement.py
+++ CarManagement.py
@@ -2,6 +2,9 @@
 
 #class CarManagement:
 
+from distutils.log import info
+
+
 def main():
     from Car import Car
     from CarList import CarList
@@ -56,6 +59,26 @@ def main():
         carinfolist.append(obj.price)
         print(carinfolist)
 
+    def searchDb(query:str):
+        
+            f = open(dbfile,'r',encoding = 'utf-8')
+            lines = []
+            printheader()
+            #if len(f.read())>0:
+
+            for y, line in enumerate(f):
+                carinfolist = list(line.split(", "))
+                
+                for x in carinfolist:
+                    if(query in x or query.upper() in x or query.lower() in x):
+                        lines.append(carinfolist)
+                #print(len(lines))
+            for i in range(len(lines)):
+                if(len(carinfolist)>0):
+                    print("|{0}| {1}| {2}| {3}| {4}|\n".format(str(lines[i][0]).center(30),str(lines[i][1]).center(30),str(lines[i][2]).center(30),str(lines[i][3]).center(30),str(lines[i][4]).strip("\n").center(30)))
+                    print('-'*160)
+            input("enter any key tpo go back to the menu ")   
+
     while 1:
         print('What would you like to do ?')
         print('1- Add a car')
@@ -65,14 +88,21 @@ def main():
         print('\n5- Quit')
         action = input('What do you want to do ?')
         if int(action)==1:
-            carName = input('Enter Car Name : ')
-            brand = input('what is the brand  : ')
-            year = input ('Year of manufacture : ')
-            price = input('Price of car : ')
-            newCar =  Car(carName,brand,year,price)
+            carName = str(input('Enter Car Name : '))
+            brand = str(input('what is the brand  : '))
+            year = str(input ('Year of manufacture : '))
+            price = str(input('Price of car : '))
+            newCar =  str(Car(carName,brand,year,price))
             #addCarToDB(newCar)
             listCars = CarList()
             listCars.addCar(newCar)
+
+        if int(action)==2:
+            print('Search for a car by any terms')
+            query = input('Enter your search query: ')
+            print('searchin for the query : '+query)
+            searchDb(query)
+
 
     
 if __name__ == "__main__":
